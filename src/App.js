bc0109d2302aeaa3401c782d0941afaaca75ecee
Mitarth Singh <70040057@PCHFLLAPR1816.local>
2021-09-16 01:03:32 +0530

    Added sign in page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HomePage from "./pages/homepage/HomePage";
 import { Route, Switch } from "react-router-dom";
 import ShopPage from "./pages/shop/ShopPage";
 import { Header } from "./components/header/Header";
+import SignInSignUp from "./pages/sign-in-sign-up/SignInSignUp";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
+        <Route exact path="/signIn" component={SignInSignUp} />
       </Switch>
     </div>
   );
---
+++ src/components/sign-in/SignIn.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import './signIn.scss'
+import FormInput from '../form-input/FormInput'
+
+export default class SignIn extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            email: '',
+            password:''
+        }
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.setState({email: '', password: ''})
+    }
+
+    handleChange = (e) => {
+        const {value, name} = e.target;
+        this.setState({[name]: value})
+    }
+
+    render() {
+        return (
+            <div className="sign-in">
+                <h2>Already have an account?</h2>
+                <span>Sign in with your email and password</span>
+                <form onSubmit={this.handleSubmit}>
+                    <FormInput 
+                      name="email" 
+                      type="email" 
+                      value={this.state.email} 
+                      onChange={this.handleChange} 
+                      label="email" 
+                      required/>
+
+                    <FormInput 
+                      name="password" 
+                      type="password" 
+                      value={this.state.password} 
+                      required 
+                      onChange={this.handleChange} label="password"/>
+                    
+
+                    <input type="submit" value='Submit form'/>
+                </form>
+            </div>
+        )
+    }
+}
