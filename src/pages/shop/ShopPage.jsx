ad0e300d614743195c977b1d5e3564c29529374a
Mitarth Singh <smitarth4@gmail.com>
2021-09-26 17:36:25 +0530

    Removed the shop local state to redux state, made a different component for data that borrows data from redux

--- src/pages/shop/ShopPage.jsx
+++ src/pages/shop/ShopPage.jsx
@@ -1,25 +1,13 @@
-import React, { Component } from 'react'
-import PreviewCollection from '../../components/preview-collection/PreviewCollection';
-import SHOP_DATA from './shopData';
+import React from 'react'
+import CollectionsOverview from '../../components/collections-overview/CollectionsOverview';
 
-export default class ShopPage extends Component {
-    constructor(){
-        super();
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-    render() {
-        const {collections} = this.state;
-        return (
-            <div className="shop-page">
-                {
-                    collections.map(({id, ...otherProps}) => (
-                        <PreviewCollection key={id}
-                        {...otherProps} />
-                    ))
-                }
-            </div>
-        )
-    }
+const ShopPage = () => {
+    return (
+        <div className="shop-page">
+            <CollectionsOverview/>
+        </div>
+    )
 }
+
+
+export default ShopPage
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -3,6 +3,8 @@ import { combineReducers } from "redux";
 import cartReducer from "./cart/cartReducer";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+import directoryReducer from "./directory/directoryReducer";
+import shopReducer from "./shop/shopReducer";
 
 const persistConfig = {
   key: "root",
@@ -13,6 +15,8 @@ const persistConfig = {
 const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
+  directory: directoryReducer,
+  shop: shopReducer,
 });
 
 export default persistReducer(persistConfig, rootReducer);
